27abac691fcae58206cc41575fda221611b44cb6
David Weikersdorfer <davidw@danvil.de>
2011-10-03 16:24:00 +0200

    Changed perlin hills generator to more or less work with plane, cylinder and sphere topologies.

--- Candy/src/Candy/Cubes/Generator/PerlinHills.h
+++ Candy/src/Candy/Cubes/Generator/PerlinHills.h
@@ -18,9 +18,15 @@ namespace Generators
 			const double cScale = 0.03;
 			const double cAmplitude = 8.0f;
 			const double cOffset = 1.0f;
-			Vec3f p = Properties::WorldToPositionCenter(cw);
-			double n = cAmplitude * (cOffset + Perlin::Noise(cScale * p[0], cScale * p[1]));
-			if(p[2] < n) {
+
+//			Vec3f p = Properties::WorldToPositionCenter(cw);
+//			double n = cAmplitude * (cOffset + Perlin::Noise(cScale * p[0], cScale * p[1]));
+//			bool is_solid = (p[2] < n);
+
+			double n = cAmplitude * (cOffset + Perlin::Noise(cScale * float(cw.x), cScale * float(cw.y)));
+			bool is_solid = (float(cw.z) < n);
+
+			if(is_solid) {
 				return CubeTypes::Mud;
 			}
 			else {
